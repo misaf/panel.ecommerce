7d019f3a6d3b02f619ed34cb70cd6e431581d53c
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-03-04 09:04:56 +0330

    update property of getSlugOptions to public access

--- app/Http/Middleware/SanitizeResponse.php
+++ app/Http/Middleware/SanitizeResponse.php
@@ -14,34 +14,34 @@ final class SanitizeResponse
     {
         $response = $next($request);
 
-        $body = $response->getContent();
+        // $body = $response->getContent();
 
-        $replace = [
-            //remove tabs before and after HTML tags
-            '/\>[^\S ]+/s'   => '>',
-            '/[^\S ]+\</s'   => '<',
-            //shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
-            '/([\t ])+/s'  => ' ',
-            //remove leading and trailing spaces
-            '/^([\t ])+/m' => '',
-            '/([\t ])+$/m' => '',
-            // remove JS line comments (simple only); do NOT remove lines containing URL (e.g. 'src="http://server.com/"')!!!
-            '~//[a-zA-Z0-9 ]+$~m' => ' ',
-            //remove empty lines (sequence of line-end and white-space characters)
-            '/[\r\n]+([\t ]?[\r\n]+)+/s'  => "\n",
-            //remove empty lines (between HTML tags); cannot remove just any line-end characters because in inline JS they can matter!
-            '/\>[\r\n\t ]+\</s'    => '><',
-            //remove "empty" lines containing only JS's block end character; join with next line (e.g. "}\n}\n</script>" --> "}}</script>"
-            '/}[\r\n\t ]+/s'             => '}',
-            '/}[\r\n\t ]+,[\r\n\t ]+/s'  => '},',
-            //remove new-line after JS's function or condition start; join with next line
-            '/\)[\r\n\t ]?{[\r\n\t ]+/s'  => '){',
-            '/,[\r\n\t ]?{[\r\n\t ]+/s'   => ',{',
-            //remove new-line after JS's line end (only most obvious and safe cases)
-            '/\),[\r\n\t ]+/s'  => '),',
-        ];
+        // $replace = [
+        //     //remove tabs before and after HTML tags
+        //     '/\>[^\S ]+/s'   => '>',
+        //     '/[^\S ]+\</s'   => '<',
+        //     //shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
+        //     '/([\t ])+/s'  => ' ',
+        //     //remove leading and trailing spaces
+        //     '/^([\t ])+/m' => '',
+        //     '/([\t ])+$/m' => '',
+        //     // remove JS line comments (simple only); do NOT remove lines containing URL (e.g. 'src="http://server.com/"')!!!
+        //     '~//[a-zA-Z0-9 ]+$~m' => ' ',
+        //     //remove empty lines (sequence of line-end and white-space characters)
+        //     '/[\r\n]+([\t ]?[\r\n]+)+/s'  => "\n",
+        //     //remove empty lines (between HTML tags); cannot remove just any line-end characters because in inline JS they can matter!
+        //     '/\>[\r\n\t ]+\</s'    => '><',
+        //     //remove "empty" lines containing only JS's block end character; join with next line (e.g. "}\n}\n</script>" --> "}}</script>"
+        //     '/}[\r\n\t ]+/s'             => '}',
+        //     '/}[\r\n\t ]+,[\r\n\t ]+/s'  => '},',
+        //     //remove new-line after JS's function or condition start; join with next line
+        //     '/\)[\r\n\t ]?{[\r\n\t ]+/s'  => '){',
+        //     '/,[\r\n\t ]?{[\r\n\t ]+/s'   => ',{',
+        //     //remove new-line after JS's line end (only most obvious and safe cases)
+        //     '/\),[\r\n\t ]+/s'  => '),',
+        // ];
 
-        $body = preg_replace(array_keys($replace), array_values($replace), $body);
+        // $body = preg_replace(array_keys($replace), array_values($replace), $body);
 
         // return $response->setContent($body);
         return $response;
