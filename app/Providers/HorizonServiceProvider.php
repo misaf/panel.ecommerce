cd3b02caaaa32d20ac1955f269469ace8916c28b
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-05-10 12:47:31 +0330

    update

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        Horizon::routeMailNotificationsTo(config('mail.from.address'));
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', fn(User $user): bool => $user->hasRole('super-admin'));
+    }
+}
