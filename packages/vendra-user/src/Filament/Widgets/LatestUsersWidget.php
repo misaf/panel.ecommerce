794f67eb7db4dfb76d4d02bd4c7c045c8545ce45
Misaf <misaf.1990@gmail.com>
2026-07-14 15:58:35 +0330

    Refactor user resources and add optional tags

---
+++ packages/vendra-user/src/Filament/Widgets/LatestUsersWidget.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Misaf\VendraUser\Filament\Widgets;
+
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Misaf\VendraUser\Models\User;
+
+final class LatestUsersWidget extends BaseWidget
+{
+    protected static ?int $sort = 8;
+
+    protected int|string|array $columnSpan = [
+        'sm' => 1,
+        'lg' => 2,
+    ];
+
+    protected function getColumns(): int
+    {
+        return 1;
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->heading(__('vendra-user::navigation.latest_users'))
+            ->query(User::query()->latest()->limit(5))
+            ->columns([
+                TextColumn::make('username')
+                    ->label(__('vendra-user::attributes.username')),
+
+                TextColumn::make('email')
+                    ->label(__('vendra-user::attributes.email'))
+                    ->searchable(),
+
+                TextColumn::make('email_verified_at')
+                    ->alignCenter()
+                    ->badge()
+                    ->extraCellAttributes(['dir' => 'ltr'])
+                    ->label(__('vendra-user::attributes.verified_at'))
+                    ->dateTime('Y-m-d H:i')
+                    ->sinceTooltip()
+                    ->unless(app()->isLocale('fa'), fn(TextColumn $column) => $column->jalaliDate('Y-m-d', toLatin: true)),
+
+                TextColumn::make('created_at')
+                    ->alignCenter()
+                    ->badge()
+                    ->extraCellAttributes(['dir' => 'ltr'])
+                    ->label(__('vendra-user::attributes.created_at'))
+                    ->dateTime('Y-m-d H:i')
+                    ->sinceTooltip()
+                    ->unless(app()->isLocale('fa'), fn(TextColumn $column) => $column->jalaliDateTime('Y-m-d', toLatin: true)),
+            ])
+            ->searchable(false)
+            ->paginated(false)
+            ->poll('10s');
+    }
+}
